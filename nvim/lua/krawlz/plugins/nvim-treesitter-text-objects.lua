617840474a2c893ee2208d519f7e6da2cf6d9787
matthew krauz <matthew.krauz@gmail.com>
2026-04-03 17:30:22 -0500

    fix(nvim): use correct module path for repeatable_move

--- nvim/lua/krawlz/plugins/nvim-treesitter-text-objects.lua
+++ nvim/lua/krawlz/plugins/nvim-treesitter-text-objects.lua
@@ -79,7 +79,7 @@ return {
       },
     })
 
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+    local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
     vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
     vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
     vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
